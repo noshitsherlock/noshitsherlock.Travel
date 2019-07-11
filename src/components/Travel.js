0f666ee04369dfa04c41f82c7ce021e0c778e223
Tommy Albinsson <tommy.albinsson@gmail.com>
2019-07-11 15:08:06 +0200

    Add pull-to-refresh

--- src/components/Travel.js
+++ src/components/Travel.js
@@ -2,8 +2,9 @@ import React, { useState, useEffect } from "react";
 import SwipeableViews from "react-swipeable-views";
 import Situation from "./Situation";
 import Departure from "./Departure";
+import ReactPullToRefresh from "react-pull-to-refresh";
 
-const useFetch = url => {
+const useFetch = (url, refresh) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -19,35 +20,58 @@ const useFetch = url => {
     // setInterval(() => {
     //   fetchData()
     // }, 60000);
-  }, []);
+  }, [refresh]);
 
   return { loading, data };
 };
 
 function Travel() {
+  const [refresh, setRefresh] = useState(false);
 
-  const { loading: travelLoading, data: travelData } = useFetch("http://192.168.86.29:5000/api/travel");
-  const { loading: situationLoading, data: situationData } = useFetch("http://192.168.86.29:5000/api/situation");
-  //const {loading:travelLoading,data:travelData} = useFetch("http://localhost:5000/api/travel");
-  //const {loading:situationLoading,data:situationData} = useFetch("http://localhost:5000/api/situation");
+  function handleRefresh(resolve, reject) {
+    setRefresh(!refresh);
+    if (true) {
+      resolve();
+    } else {
+      reject();
+    }
+  };
+
+  //const { loading: travelLoading, data: travelData } = useFetch("http://192.168.86.29:5000/api/travel");
+  //const { loading: situationLoading, data: situationData } = useFetch("http://192.168.86.29:5000/api/situation");
+  const { loading: travelLoading, data: travelData } = useFetch("http://localhost:5000/api/travel", refresh);
+  const { loading: situationLoading, data: situationData } = useFetch("http://localhost:5000/api/situation", refresh);
 
   return (
-    <SwipeableViews enableMouseEvents>
-      <div className="site">
-        <h1><i className="icon ion-md-bus"></i> Linje 134</h1>
-        {travelLoading
-          ? <div>Loading...</div>
-          : <Departure data={travelData}></Departure>
-        }
+    <ReactPullToRefresh
+      onRefresh={handleRefresh}>
+      <div id="ptr">
+        <div className="loading">
+          <span id="l1"></span>
+          <span id="l2"></span>
+          <span id="l3"></span>
+        </div>
       </div>
-      <div>
-        {situationLoading
-          ? <div>Loading...</div>
-          : <Situation data={situationData}></Situation>
-        }
 
+      <div id="content">
+        <SwipeableViews enableMouseEvents>
+          <div className="site">
+            <h1><i className="icon ion-md-bus"></i> Linje 134</h1>
+            {travelLoading
+              ? <div>Loading...</div>
+              : <Departure data={travelData}></Departure>
+            }
+          </div>
+          <div>
+            {situationLoading
+              ? <div>Loading...</div>
+              : <Situation data={situationData}></Situation>
+            }
+
+          </div>
+        </SwipeableViews>
       </div>
-    </SwipeableViews>
+    </ReactPullToRefresh>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import './custom.css';
+import './refresh.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
