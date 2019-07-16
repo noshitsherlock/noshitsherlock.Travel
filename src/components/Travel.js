af182aebbc3d582359114ee5999ab6b53d5f15ec
Tommy Albinsson <tommy.albinsson@gmail.com>
2019-07-16 15:30:02 +0200

    First implementation of getting from firebase

--- src/components/Travel.js
+++ src/components/Travel.js
@@ -20,77 +20,84 @@ const useFetch = (url, refresh) => {
 
   useEffect(() => {
     fetchData()
-    // setInterval(() => {
-    //   fetchData()
-    // }, 60000);
   }, [refresh]);
 
   return { loading, data };
 };
 
-function Travel() {
-  let siteIdsQueryString = "siteids=1532&siteids=9119";
-  const [refresh, setRefresh] = useState(false);
+function getUserData() {
+  return new Promise(function (resolve, reject) {
+    Firebase.database()
+      .ref("/")
+      .on("value", snapshot => {
+        const state = snapshot.val();
+        console.log("From promise:" + state);
+        resolve(state);
+      })
+  });
+};
 
+function Travel() {
+  const [siteIds, setSiteIds] = useState("");
+  const [refresh, setRefresh] = useState(false);
 
   const writeData = () => {
     Firebase.database()
       .ref("/")
-      .set("siteids=1532&siteids=9119");
+      .set("siteids=1532&siteids=9119").then(function() {
+        setSiteIds("siteids=1532&siteids=9119");
+        setRefresh(!refresh);
+      });
+    //.set("siteids=1532&siteids=9119&siteids=9192");
 
-      setSiteIds("siteids=1532&siteids=9119");
+    //setSiteIds("siteids=1532&siteids=9119&siteids=9192");
+
     console.log("DATA SAVED");
   };
-  
-  const getUserData = () => {
-    let ref = Firebase.database().ref("/");
-    ref.on("value", snapshot => {
-      const state = snapshot.val();
-      console.log(state);
-      setSiteIds(state);
+
+  useEffect(() => {
+    getUserData().then(function (value) {
+      if(siteIds != value) setSiteIds(value);
+          
+      setRefresh(true);
+      console.log("From then");
     });
-  };
+  });
 
-  const [siteIds, setSiteIds] = useState(getUserData);
-
-  console.log(siteIds);
-
-  const { loading: travelLoading, data: travelData } = useFetch(config.url.API_URL_TRAVEL_MULTIPLE + siteIdsQueryString, refresh);
+  const { loading: travelLoading, data: travelData } = useFetch(config.url.API_URL_TRAVEL_MULTIPLE + siteIds, refresh);
   const { loading: situationLoading, data: situationData } = useFetch(config.url.API_URL_SITUATION, refresh);
-  
-
 
   return (
+    siteIds.length > 0 && !travelLoading && !situationLoading &&
     <Pullable onRefresh={() => setRefresh(!refresh)}>
-        <div>
-          {
-            travelLoading
-              ? <div>Loading...</div>
-              : <SwipeableViews enableMouseEvents>
-                {
-                  travelData.map(travel =>
-                    <Departure key={uuid.v4()} data={travel}></Departure>
-                  )
-                }
-                 { <div>
-                   {
-                     situationLoading
-                     ? <div>Loading...</div>
-                     : <Situation data={situationData}></Situation>
-                   }
+      <div>
+        {
+          travelLoading
+            ? <div>Loading...</div>
+            : <SwipeableViews enableMouseEvents>
+              {
+                travelData.map(travel =>
+                  <Departure key={uuid.v4()} data={travel}></Departure>
+                )
+              }
 
-                 </div> }
-                 <div>
-                   <button onClick={writeData}>PRESS</button>
-                   <button onClick={getUserData}>GET DATA</button>
+              {
+                situationLoading
+                  ? <div>Loading...</div>
+                  : <Situation data={situationData}></Situation>
+              }
 
-                   <p>SiteIds: {siteIds}</p>
-                 </div>
-              </SwipeableViews>
-          }
+              <div>
+                <button onClick={writeData}>PRESS</button>
+                <button onClick={getUserData}>GET DATA</button>
 
-        </div>
-      </Pullable>
+                <p>SiteIds: {siteIds}</p>
+              </div>
+            </SwipeableViews>
+        }
+
+      </div>
+    </Pullable>
   )
 }
 
