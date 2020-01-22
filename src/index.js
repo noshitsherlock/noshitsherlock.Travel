f8f790e971e06aa3f7b795377cf962a1b6afca22
Tommy Albinsson <tommy.albinsson@gmail.com>
2020-01-22 19:10:02 +0100

    Add black background on splashscreen

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 //import 'bootstrap/dist/css/bootstrap.css';
-import './dark-mode.css'
+import './dark-mode.css'; // comment this out, and take back bootstrap above to get back to default
 import './custom.css';
 // import './refresh.css';
 
