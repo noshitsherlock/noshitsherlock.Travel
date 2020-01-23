57906833ad0e2478c3ee20c69eb179d540eed294
Tommy Albinsson <tommy.albinsson@gmail.com>
2020-01-23 16:05:42 +0100

    Implement material ui for every component

--- src/components/Situation.js
+++ src/components/Situation.js
@@ -1,21 +1,63 @@
 import React from "react";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { deepOrange } from '@material-ui/core/colors';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        width: '100%',
+        maxWidth: 600,
+        backgroundColor: theme.palette.background.paper,
+        marginTop: 5,
+    },
+    large: {
+        color: theme.palette.getContrastText(deepOrange[500]),
+        backgroundColor: deepOrange[500],
+        width: theme.spacing(7),
+        height: theme.spacing(7),
+        textAlign: "center",
+        marginRight: "10px"
+    },
+    inline: {
+        display: 'inline',
+    },
+}));
 
 function Situation({ data }) {
+
+    const classes = useStyles();
+
     return (
-        <ul className="list-group">
+        <List className={classes.root}>
             {JSON.parse(data).ResponseData.TrafficTypes.map(types =>
-                <li key={types.Id} className="list-group-item">
-                    <div className="d-flex w-100 justify-content-between">
-                        <h5 className="mb-1">{types.Name}</h5>
-                        {/* <small>3 days ago</small> */}
-                    </div>
-                    {types.Events.map(event =>
-                        <p key={event.EventId} className="mb-1">{event.Message}</p>
-                    )}
-                    {/* <small>Donec id elit non mi porta.</small> */}
-                </li>
+                <div>
+                    <ListItem key={types.Id}>
+                    <ListItemText
+                        primary={<Typography variant="h5">{types.Name}</Typography>}
+                        secondary={
+                            types.Events.map(event =>
+                            <React.Fragment key={event.EventId}>
+                            <Typography
+                                component="span"
+                                variant="body2"
+                                className={classes.inline}
+                                color="textPrimary"
+                            >
+                                {event.Message}
+                            </Typography>                            
+                            </React.Fragment>
+                            )
+                        }
+                        />
+                    </ListItem>    
+                    <Divider />                
+                </div>                
             )}
-        </ul>
+        </List>
     )
 }
 
