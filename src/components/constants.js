b3cd06bbd39b5117503b490680b097dbfa97ddba
Tommy Albinsson <tommy.albinsson@gmail.com>
2020-03-04 20:48:48 +0100

    Adding missing part in api url

--- src/components/constants.js
+++ src/components/constants.js
@@ -6,7 +6,7 @@ const prod = {
         // API_URL_TRAVEL_MULTIPLE: "http://192.168.86.29:5000/api/travelmultiplesiteid?" + sites.Ids,
         // API_URL_SITUATION: "http://192.168.86.29:5000/api/situation"
         API_URL_TRAVEL: "https://noshitsherlock-travel-api.azurewebsites.net/api/travel",
-        API_URL_TRAVEL_MULTIPLE: "https://noshitsherlock-travel-node-api.azurewebsites.net/" + sites.Ids + "/timewindow/30",
+        API_URL_TRAVEL_MULTIPLE: "https://noshitsherlock-travel-node-api.azurewebsites.net/departures/site/" + sites.Ids + "/timewindow/30",
         API_URL_SITUATION: "https://noshitsherlock-travel-node-api.azurewebsites.net/situation"
     }
 };
